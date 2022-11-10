3057d4fe44ca3c17e7be70a33a176a92ef7cfc2c
JJH030415 <115202133+JJH030415@users.noreply.github.com>
2022-11-10 15:41:27 +0900

    Update 코테 입문.c

--- 코테 입문.c
+++ 코테 입문.c
@@ -1,58 +1,77 @@
 #include <stdio.h>
-// 두수의 곱  
-// void main() 
-// {
-// 	int num1=3,num2=4; 
-// 	printf("Result:%d*%d=%d\n",num1,num2,num1*num2);  
+몫 구하기 
+void main() 
+{
+	int num1=10,num2=5; 
+	printf("Result:%d/%d=%d",num1,num2,num1/num2); 
+	
+	printf("\n"); 		
+} 
+
+
+두수의 곱  
+void main() 
+{
+	int num1=3,num2=4; 
+	printf("Result:%d*%d=%d\n",num1,num2,num1*num2);  
 	 
-// 	printf("\n"); 
-// }  
+	printf("\n"); 
+}  
 
 
-// 몫 구하기 
-// void main() 
-// {
-// 	int num1=10,num2=5; 
-// 	printf("Result:%d/%d=%d",num1,num2,num1/num2); 
+
+ 
+
+두수의 차 
+void main() 
+{
+	int num1=2,num2=3; 
+	printf("Result:%d-%d=%d",num1,num2,num1-num2); 
 	
-// 	printf("\n"); 		
-// } 
+	printf("\n"); 		
+} 
  
 
-// 두수의 차 
-// void main() 
-// {
-// 	int num1=2,num2=3; 
-// 	printf("Result:%d-%d=%d",num1,num2,num1-num2); 
-	
-// 	printf("\n"); 		
-// } 
+나이 출력  
+void main() 
+{
+	int age;
+	int year = 2022; 
+	scanf("%d",&age);  
+	if(age==40) 
+	{
+	print("%d년 기준 %d살이므로 1983년생입니다.",age,year);  	 
+	}
+} 
+
+
+나머지 구하기  
+void main() 
+{
+	int num1=4,num2=3;	 
+	printf("%d/%d=%d",num1,num2,num1%num2); 
+}
  
 
-// //나이 출력  
-// void main() 
-// {
-// 	int age;
-// 	int year = 2022; 
-// 	scanf("%d",&age);  
-// 	if(age==40) 
-// 	{
-// 	print("%d년 기준 %d살이므로 1983년생입니다.",age,year);  	 
-// 	}
-// } 
+숫자 비교하기  
+void main() 
+{
+	int num1=2,num2=2;
+	if(num1==num2)
+	{
+		printf("%d",-1); 
+	}
+	else  
+	{
+		printf("%d",1); 	
+	}
+}
 
 
- 
-//숫자 비교하기  
-//void main() 
-//{
-//	int num1=2,num2=2;
-//	if(num1==num2)
-//	{
-//		printf("%d",-1); 
-//	}
-//	else  
-//	{
-//		printf("%d",1); 	
-//	}
-//}
+두수의 합  
+void main() 
+{
+	int num1,num2;
+	scanf("%d %d",&num1,&num2);  
+	printf("%d+%d=%d",num1,num2,num1+num2); 	
+} 
