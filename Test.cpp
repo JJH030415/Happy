401718e2dca8bb3579332c5af302539663b05c64
JJH030415 <115202133+JJH030415@users.noreply.github.com>
2022-11-10 14:14:17 +0900

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std; 
+
+//int main()     //1번 문제 
+//{
+//	int age = 5;
+//	cout << "제 나이는" <<age<<"살입니다" << endl; 
+//} 
+
+
+
+//int main(void)     //2번 문제  
+//{
+//	int num; 
+//	for(num=1;num<=100;num++)
+//	{
+//		if (num%3==0&&num%5==0)
+//		cout<<"3과5의 공배수들입니다"<<num;
+//	}
+//	return 0;
+//}   
+
+
+
+//int main()   // 3번 문제 
+//{
+//	int a = 5; 
+//	for(int i=1; i<=5; i++,a--) 
+//	{
+//		for(int j=1; j<=5;j++) 
+//		{
+//			if(i==j || a==j) 
+//			{
+//				cout << "*";			
+//			}
+//			else
+//				cout << " "; 
+//		}
+//		cout << "\n";  
+//	}
+//}
+
+
+
+
+//int main()  //4번문제  
+//int main()  
+//{
+//    int n,m;
+// 
+//    cout << "Enter n and m : ";
+//    cin >> n; 
+//    cin >> m;
+// 
+//    for (int i = 2; i<5; i++) {
+//        for (int j=1; j<10; j++)
+//        cout << i << " x " << j << " = " << i * j << "\t"<<endl;
+//        cout<<endl;
+//    }
+//	return 0;
+//}
+	  
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+
